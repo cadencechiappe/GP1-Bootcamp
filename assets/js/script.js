92ae54977f050412162b741047cf8635e5abfd93
Abraham <abraham_barajas13@hotmail.com>
2022-04-18 23:18:54 -0500

    added flair/j

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,20 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
+//When the User clicks on <btn>, opens the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
+
+//When the user clicks outside the open modal, close the modal
+window.onclick = function(event) {
+  if (event.target == modal) {
+      modal.style.display = "none";
+  }
+}
+
